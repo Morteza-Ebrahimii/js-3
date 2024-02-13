0eab9a6481bf9192efdcef3628d795126c7c1915
morteza Ebrahimi <morteza.nsrn@gmail.com>
2024-02-13 14:27:33 +0330

    feat : add-homework-js-24

---
+++ js-24-answer-question/JS-24-Q-1.js
@@ -0,0 +1,30 @@
+//? Question-3: Follow these steps:
+//todo-1: Write a Factory function (function name must be: createPerson).
+//todo-2: The createPerson factory function accepts three parameters: name, age, and profession
+//todo-3: Define a method (method name must be: introduce) on the object. This method must return this structure: "Hi, my name is (name). I'm (age) years old and work as a (profession)."
+//todo-4: Then display below outputs using the function createPerson with these parameters: (name: John Doe, age: 30, profession: Web Developer, or our personal information).
+/**
+ * Output: John Doe
+ * Output: 30
+ * Output: Web Developer
+ * Output: Hi, my name is John Doe. I'm 30 years old and work as a Web Developer.
+ *
+ */
+
+//!Answer:
+
+const makeUser = (userName, age, profession) => {
+  const user = {};
+
+  user.name = userName;
+  user.poersonAge = age;
+  user.personProfession = profession;
+
+  user.introduce = function () {
+    return `Hi, my name is ${userName}. I'm ${age} years old and work as a ${profession}.`;
+  };
+  return user;
+};
+
+const person = makeUser("John Doe", "30", "Web Developer");
+console.log(person.introduce());
